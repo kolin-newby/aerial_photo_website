21706f04a8de044185e4115d01b5bc3c1393deb2
kolin-newby <kone9079@colorado.edu>
2020-01-07 16:53:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static(__dirname + '/public'));
 
 // home page
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + 'public/views/home.html')
+	res.sendFile(__dirname + '/public/views/home.html')
 })
 
 // console.log(__dirname);
