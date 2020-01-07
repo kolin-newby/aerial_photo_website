b769d72ef20fd0d840f3bc11e3654cdc32e5123f
kolin-newby <kone9079@colorado.edu>
2020-01-07 12:00:22 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // Add the express framework
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
-const app = express();
+var app = express();
 
 //Create Database Connection
 // ****
@@ -12,16 +13,17 @@ const app = express();
 console.log(__dirname)
 
 //set static folder
-app.use(express.static(path.join(__dirname + 'public')));
+app.use(express.static('public'));
+// app.use(bodyParser());
 
 
 // home page
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'views', 'home.html'));
-// });
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'views', 'home.html'));
+});
 
 // app.get('/', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'views', 'events.html'));
+// 	res.sendFile(path.join(__dirname, 'public', 'views', 'events.html'));
 // });
 
 const port = 2048;
