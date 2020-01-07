b75ddaf2fb20bf1b29402d4bc71295195b12994c
kolin-newby <kone9079@colorado.edu>
2020-01-07 15:12:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
-// Add the express framework
-const express = require('express');
-const path = require('path');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
+const express = require('express'); // Add the express framework
+let app = express();
 
+const bodyParser = require('body-parser'); // Add the body-parser tool
+app.use(bodyParser.json());              // Add support for JSON encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
+
+const pug = require('pug'); // Add pug
+
+//sets pug as view engine
 app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/'));
 
-var app = express();
-app.use(helmet());
-
-//Create Database Connection
-// ****
-//db config
-// ****
-
-// console.log(__dirname)
-
-// //set static folder
-// app.use(express.static('public/views'));
-// app.use(express.static('public/resources'));
-// // app.use(bodyParser());
-
-
-// // home page
+// home page
 app.get('/', (req, res) => {
-	response.render('home');
-	// res.sendFile(path.join(__dirname + 'public' + 'views' + 'home.html'));
-});
+	response.render('home')
+})
 
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.join(__dirname + 'public' + 'views' + 'events.html'));
-// });
-
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.join(__dirname + 'public' + 'views' + 'realty_comm.html'));
-// });
-
-const port = 2048;
-app.listen(port, () => {
-	console.log('listening on port ' + port);
-});
+var port = 2048;
+app.listen(port);
+console.log('listening on port ' + port);
