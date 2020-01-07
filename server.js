8fc9593633a3a0b28fe9df3860a5ca87cd5ab341
kolin-newby <kone9079@colorado.edu>
2020-01-07 16:52:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ let app = express();
 app.use(express.static(__dirname + '/public'));
 
 // home page
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
 	response.sendFile(__dirname + 'public/views/home.html')
 })
 
-console.log(__dirname);
+// console.log(__dirname);
 
 var port = 2048;
 app.listen(port, () => {
