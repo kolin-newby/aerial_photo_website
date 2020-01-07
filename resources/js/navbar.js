4204521720d90afdfcb5f16f8b58c59affb17be6
kolin-newby <kone9079@colorado.edu>
2020-01-06 17:19:01 -0700

    finished responsive features, made updates to pages

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,9 +1,13 @@
-function navfunction(){
-	var x = document.getElementById("nav_bar");
-	if (x.className === "navbar"){
-		x.className += " responsive";
-	}
-	else{
-		x.className = "navbar";
-	}
+function navFunction(){
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function navCollapse(){
+	var x = document.getElementById("myTopnav");
+	x.className = "topnav";
 }
