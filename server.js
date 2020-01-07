d1e5b157c82ffd66b7d1c199187c7f2b84fd1ced
kolin-newby <kone9079@colorado.edu>
2020-01-07 16:35:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,14 @@ const bodyParser = require('body-parser'); // Add the body-parser tool
 app.use(bodyParser.json());              // Add support for JSON encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
 
-const pug = require('pug'); // Add pug
-
-//sets pug as view engine
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 
 // home page
-app.get('/', (req, res) => {
-	response.render('home.pug')
-})
+// app.get('/home', (req, res) => {
+// 	response.render('home.html')
+// })
 
 var port = 2048;
-app.listen(port);
-console.log('listening on port ' + port);
-
-//test
+app.listen(port, => {
+	console.log('listening on port ' + port);
+};
