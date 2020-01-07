2dd2385988273245c62023229adc075877652337
kolin-newby <kone9079@colorado.edu>
2020-01-07 16:43:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express'); // Add the express framework
 let app = express();
 
-const bodyParser = require('body-parser'); // Add the body-parser tool
-app.use(bodyParser.json());              // Add support for JSON encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // Add support for URL encoded bodies
-
 app.use(express.static(__dirname + '/public'));
 
 // home page
-// app.get('/home', (req, res) => {
-// 	response.render('home.html')
-// })
+app.get('/home', (req, res) => {
+	response.sendFiles(__dirname + 'views/home.html')
+})
 
 var port = 2048;
 app.listen(port, () => {
