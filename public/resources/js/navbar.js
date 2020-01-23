ed565b8eec91bd8d6373cc19121871abed8c9cf2
kolin-newby <kone9079@colorado.edu>
2020-01-23 15:43:03 -0700

    added start to collabsible contact form

--- public/resources/js/navbar.js
+++ public/resources/js/navbar.js
@@ -11,3 +11,17 @@ function navCollapse(){
 	var x = document.getElementById("myTopnav");
 	x.className = "topnav";
 }
+
+function contactCollapse(){
+	var x = document.getElementById("contactCollapse");
+	x.addEventListener("click", function(){
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if(content.style.display === "block") {
+			content.style.display = "none";
+		}
+		else{
+			content.style.display = "block";
+		}
+	});
+}
