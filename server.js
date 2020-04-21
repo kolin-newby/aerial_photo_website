814061a96d4124f791aa32ff7aa7c3e55972b9d4
Kolin Newby <kolin.newby@gmail.com>
2020-04-21 12:13:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,44 @@ const express = require('express'); // Add the express framework
 
 const helmet = require('helmet'); //add helmet for some security
 
-let app = express();
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const app = express();
+
+ //certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/newbyap.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/newbyap.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/newbyap.com/chain.pem', 'utf8');
+
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+//---------------------------
+
 
 app.use(helmet());
 
-app.use(express.static(__dirname + '/public'));
+
+//starting both http and https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+
+
+app.use(express.static(__dirname + '/public', { dotfiles: 'allow'} ));
 
 // home page
 
@@ -35,7 +68,7 @@ app.get('/gallery.html', (req, res) => {
 	res.sendFile(__dirname + '/public/views/gallery.html')
 })
 
-var port = 2048;
+var port = 80;
 app.listen(port, () => {
 	console.log('listening on port ' + port);
 });
