332e3a3c7287f0c69407bafadae61d3ba7e3e841
Kolin Newby <kolin.newby@gmail.com>
2020-05-03 17:09:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -50,15 +50,15 @@ http.createServer(app).listen(80, () => {
 })
 
 
-https.createServer({
-	key: fs.readFileSync('/etc/letsencrypt/live/newbyap.com/privkey.pem'),
+// https.createServer({
+// 	key: fs.readFileSync('/etc/letsencrypt/live/newbyap.com/privkey.pem'),
 
-	cert: fs.readFileSync('/etc/letsencrypt/live/newbyap.com/cert.pem'),
+// 	cert: fs.readFileSync('/etc/letsencrypt/live/newbyap.com/cert.pem'),
 
-	ca: fs.readFileSync('/etc/letsencrypt/live/newbyap.com/chain.pem')
-}, app).listen(port, () => {
-	console.log('Listening on port ' + port)
-})
+// 	ca: fs.readFileSync('/etc/letsencrypt/live/newbyap.com/chain.pem')
+// }, app).listen(port, () => {
+// 	console.log('Listening on port ' + port)
+// })
 
 // app.listen(port, () => {
 // 	console.log('listening on port ' + port);
