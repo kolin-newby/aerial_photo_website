01d2538edbbfe262cc5d691ce5761d0858ccb368
Kolin Newby <kolin.newby@gmail.com>
2020-05-13 12:15:56 -0600

    finished responsive navbar

--- public/resources/js/navbar.js
+++ public/resources/js/navbar.js
@@ -1,14 +1,19 @@
 function navFunction(){
   var x = document.getElementById("main_navbar");
+  var bars = document.getElementById("menu bar icon");
   if (x.className === "menu") {
     x.className += " responsive";
+    bars.className = "menu dropdown-icon fas fa-chevron-up";
   } else {
     x.className = "menu";
+    bars.className = "menu dropdown-icon fas fa-bars";
   }
   // console.log('nav_test')
 }
 
 function navCollapse(){
 	var x = document.getElementById("main_navbar");
+	var bars = document.getElementById("menu bar icon");
 	x.className = "menu";
+	bars.className = "menu dropdown-icon fas fa-bars";
 }
